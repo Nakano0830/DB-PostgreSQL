88a5fbea1be658b5bd316fea527a7ed354ee1c75
Nakano1234 <206956194+Nakano0830@users.noreply.github.com>
2025-11-01 17:15:19 +0900

    Update ex-02_4

--- sql/05/drill/ex-02_4.sql
+++ sql/05/drill/ex-02_4.sql
@@ -9,15 +9,17 @@ SELECT
 FROM
     s_characters
 WHERE
-    CAST('2025-10-15' AS DATE) - CAST(last_login_at AS DATE) >= 10;
+    CAST('2025-10-15' AS DATE) - CAST(last_login_at AS DATE) <= 10
+ORDER BY
+    id;
 
 UPDATE s_characters
 SET
-    buff = buff + CASE
-        WHEN ('2025-10-15' AS DATE) - CAST(last_login_at AS DATE) >= 60 THEN 0.40
-        WHEN ('2025-10-15' AS DATE) - CAST(last_login_at AS DATE) >= 40 THEN 0.30
-        WHEN ('2025-10-15' AS DATE) - CAST(last_login_at AS DATE) >= 20 THEN 0.20
-        WHEN ('2025-10-15' AS DATE) - CAST(last_login_at AS DATE) >= 10 THEN 0.10
+    buff = CASE
+        WHEN (CAST('2025-10-15' AS DATE) - CAST(last_login_at AS DATE)) >= 60 THEN 0.40
+        WHEN (CAST('2025-10-15' AS DATE) - CAST(last_login_at AS DATE)) >= 40 THEN 0.30
+        WHEN (CAST('2025-10-15' AS DATE) - CAST(last_login_at AS DATE)) >= 20 THEN 0.20
+        WHEN (CAST('2025-10-15' AS DATE) - CAST(last_login_at AS DATE)) >= 10 THEN 0.10
         ELSE 0.00
     END;
 
